9c7aa8fc0b8902d235494082d0839e94d7cde2a0
Krishna <marathek183@gmail.com>
2025-12-14 15:11:33 +0530

    Secure env setup, updated .gitignore, removed pycache, updated DB config

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Jignesh%23087@localhost:5432/reconciliation_db"
+# Load environment variables from .env file
+load_dotenv(dotenv_path="../.env")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
